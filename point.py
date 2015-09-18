a11703d83920f747cd9e0ecdb912be54a49e59ba
SeanGrady <vedicmonk@gmail.com>
2015-09-17 23:41:58 -0700

    New branch for point class & tests. We'll need to change some other files to use points anywhere, but I'm afraid to do that now because we have so many of the git branches and merge conflicts scare and anger the monkey who lives in my head. I've made the point class such that it can be used as a key in a dictionary because I wanted to see if I could and it occured to me that maybe the board could be a dict where the keys are coordinates (i.e. Points).

---
+++ point.py
@@ -0,0 +1,22 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return "({}, {})".format(self.x, self.y)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, point):
+        return (self.x, self.y) == (point.x, point.y)
+
+    def __add__(self, point):
+        return Point(self.x + point.x, self.y + point.y)
+
+    def __neg__(self):
+        return Point(-self.x, -self.y)
+
+    def __sub__(self, point):
+        return self + -point
